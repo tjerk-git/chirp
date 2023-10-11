6e44e7ad7f34109265846fc56483368408389225
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2023-10-11 14:42:29 +0200

    removed this thing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import { api } from "~/utils/api";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
-  const user = useUser();
 
   return (
     <>
@@ -16,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!!user.isSignedIn && SignOutButton}
+
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
